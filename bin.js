c6b4b7884b620c1edfce4224d65dc0af3824ff98
lachrist <lachrist@vub.ac.be>
2017-10-18 15:15:29 +0200

    fix case where only 1 child or 1 parent

--- bin.js
+++ bin.js
@@ -2,6 +2,7 @@
 const Minimist = require("minimist");
 const Main = require("./main.js");
 
+const toarray = (x) => Array.isArray(x) ? x : [x]
 const options1 = Minimist(process.argv.slice(2));
 const options2 = {
   basedir: options1.basedir || process.cwd(),
@@ -12,13 +13,13 @@ Main({
   path: options1["spawn-path"],
   type: options1["spawn-type"],
   options: options2
-}, options1["parent-path"].map((path, index) => ({
+}, toarray(options1["parent-path"]).map((path, index) => ({
   path: path,
-  type: options1["parent-type"][Math.min(index, options1["parent-type"].length-1)],
+  type: toarray(options1["parent-type"])[index],
   options: options2
-})) ,options1["child-path"].map((path, index) => ({
+})), toarray(options1["child-path"]).map((path, index) => ({
   path: path,
-  type: options1["child-type"][Math.min(index, options1["child-type"].length-1)],
+  type: toarray(options1["child-type"])[index],
   options: options2
 })), (error, script) => {
   if (error)
